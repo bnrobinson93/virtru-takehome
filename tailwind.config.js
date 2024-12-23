e53c7029743ed7acbe1656b294d19bba59f2229d
Brad R <31802085+bnrobinson93@users.noreply.github.com>
2024-12-23 11:30:23 -0600

    Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,44 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        black: '#000',
+        white: '#fff',
+        background: "#000",
+        foreground: "#fff",
+        accent: "#001e4a",
+        secondary: {
+          DEFAULT: "#eaedf1",
+          50: '#FFFFFF', 
+          100: '#FFFFFF', 
+          200: '#FAFCFC', 
+          300: '#F7F9FA', 
+          400: '#F2F5F7', 
+          500: '#eaedf1', 
+          600: '#BFCBDB', 
+          700: '#8497B5', 
+          800: '#546A91', 
+          900: '#30446E', 
+          950: '#142347'
+        },
+        primary: {
+          DEFAULT: "#001e4a",
+          50: '#E9F1F5', 
+          100: '#D5E5ED', 
+          200: '#9DBDD1', 
+          300: '#6E99B8', 
+          400: '#265180', 
+          500: '#001E4A', 
+          600: '#001942', 
+          700: '#001438', 
+          800: '#000E2B', 
+          900: '#000921', 
+          950: '#000514'
+        },
+      },
+    },
   },
   plugins: [],
 }
