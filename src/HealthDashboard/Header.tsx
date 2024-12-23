6f8354227b50b6a1cf1139c7327940e1c84381ba
Brad R <31802085+bnrobinson93@users.noreply.github.com>
2024-12-23 17:56:27 -0600

    Style changes

--- src/HealthDashboard/Header.tsx
+++ src/HealthDashboard/Header.tsx
@@ -1,28 +1,35 @@
 import { Filter, Pause, Play } from "lucide-react";
-import { Button } from '@/components/ui/button';
+import { Button } from "@/components/ui/button";
 
-type Props = {pause: () => void, resume: () => void, paused: boolean}
+type Props = { pause: () => void; resume: () => void; paused: boolean };
 
-function Header({pause, resume, paused}: Props) {
+function Header({ pause, resume, paused }: Props) {
   const action = paused ? resume : pause;
   const buttonText = paused ? "Resume Updates" : "Pause Updates";
   const Icon = paused ? <Play size={24} /> : <Pause size={24} />;
 
   return (
-      <header className="flex gap-2 items-center justify-between top-0 px-4 py-2 bg-gray-100 dark:bg-gray-900 h-14">
-        <div className="font-bold text-3xl">Health Check</div>
-        <nav>
-          <Button variant="secondary" className="text-primary-500 hover:text-primary-950 px-2">
-            <Filter />
-            <span className="hidden sm:inline-block -ml-1">Filter</span>
-          </Button>
-          <Button variant="secondary" className="text-primary-500 hover:text-primary-950 px-2" onClick={action}>
-            {Icon}
-            <span className="hidden sm:inline-block -ml-1">{buttonText}</span>
-          </Button>
-        </nav>
-      </header>
-  )
+    <header className="top-0 flex h-14 items-center justify-between gap-2 bg-gray-400 px-4 py-2 dark:bg-gray-600">
+      <div className="text-3xl font-bold">Health Check</div>
+      <nav>
+        <Button
+          variant="secondary"
+          className="px-2 text-primary-500 hover:text-primary-950"
+        >
+          <Filter />
+          <span className="-ml-1 hidden sm:inline-block">Filter</span>
+        </Button>
+        <Button
+          variant="secondary"
+          className="px-2 text-primary-500 hover:text-primary-950"
+          onClick={action}
+        >
+          {Icon}
+          <span className="-ml-1 hidden sm:inline-block">{buttonText}</span>
+        </Button>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
