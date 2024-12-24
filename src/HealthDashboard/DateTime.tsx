e8c14e218bc96533d3e2a1386db670b6e6620360
Brad R <31802085+bnrobinson93@users.noreply.github.com>
2024-12-23 20:54:20 -0600

    Fix for using context

--- src/HealthDashboard/DateTime.tsx
+++ src/HealthDashboard/DateTime.tsx
@@ -1,5 +1,12 @@
+import { useContext } from "react";
+import StatusContext from "@/contexts/Statuses";
+
 function DateTime() {
-  const now = new Date();
+  const context = useContext(StatusContext);
+
+  let now = new Date();
+  if (context) now = new Date(context.oldTimestamp);
+
   const displayTime = `${now.toDateString()} ${now.toLocaleTimeString()}`;
   return <div className="self-end text-xs">Refresh time: {displayTime}</div>;
 }
