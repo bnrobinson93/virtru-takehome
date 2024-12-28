d3c73b047ddb9139a3341fdec7eb18923d7ddadf
Brad R <31802085+bnrobinson93@users.noreply.github.com>
2024-12-28 17:26:37 -0600

    Better show when fetching is paused

--- src/HealthDashboard/Header.tsx
+++ src/HealthDashboard/Header.tsx
@@ -1,5 +1,6 @@
 import { Filter, Pause, Play } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 type Props = { pause: () => void; resume: () => void; paused: boolean };
 
@@ -9,11 +10,18 @@ function Header({ pause, resume, paused }: Props) {
   const Icon = paused ? <Play size={24} /> : <Pause size={24} />;
 
   return (
-    <header className="top-0 mb-4 flex h-14 items-center justify-between gap-2 bg-gray-400 px-4 py-2 dark:bg-gray-600">
+    <header
+      className={cn(
+        "top-0 mb-4 flex h-14 items-center justify-between gap-2 bg-gray-400 px-4 py-2 shadow-md transition-all fade-out-60 dark:bg-gray-600",
+        {
+          "bg-pink-100": paused,
+        },
+      )}
+    >
       <div className="text-3xl font-bold">Health Check</div>
       <nav>
         <Button
-          variant="secondary"
+          variant="ghost"
           className="px-2 text-primary-500 hover:text-primary-950"
           onClick={action}
         >
@@ -21,7 +29,7 @@ function Header({ pause, resume, paused }: Props) {
           <span className="-ml-1 hidden sm:inline-block">{buttonText}</span>
         </Button>
         <Button
-          variant="secondary"
+          variant="ghost"
           className="px-2 text-primary-500 hover:text-primary-950"
         >
           <Filter />
