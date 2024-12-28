ccfaa141096d95a667196eb63b5ce324bf10ac68
Brad R <31802085+bnrobinson93@users.noreply.github.com>
2024-12-27 18:58:33 -0600

    Tooltip for message

--- src/HealthDashboard/ItemizedStatus/ListItem.tsx
+++ src/HealthDashboard/ItemizedStatus/ListItem.tsx
@@ -3,47 +3,58 @@ import { statusColorBg } from "@/lib/statusColor";
 import { cn } from "@/lib/utils";
 import HideItem from "./HideItem";
 import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { Share2 } from "lucide-react";
-import { useState } from "react";
 
 type Props = {
   serviceName: string;
-  status: Status;
+  status: ServiceStatus;
   checked: { [K in string]: boolean };
   setChecked: React.Dispatch<React.SetStateAction<{ [K in string]: boolean }>>;
 };
 
 function ListItem({ serviceName, status, checked, setChecked }: Props) {
-  const color = statusColorBg(status);
+  const color = statusColorBg(status.status);
 
   return (
-    <li key={serviceName} className="flex items-center justify-between p-2">
-      <div className="flex items-center space-x-4">
-        <Checkbox
-          id={serviceName}
-          checked={checked[serviceName]}
-          onCheckedChange={(checkedVal) =>
-            setChecked((old) => ({ ...old, [serviceName]: !!checkedVal }))
-          }
-        />
-        <span className={cn("h-3 w-3 rounded-full", color)}>
-          <span className="sr-only">{status}</span>
-        </span>
-        <label htmlFor={serviceName} className="first-letter:uppercase">
-          {serviceName}
-        </label>
-        <span className="hidden text-xs italic text-gray-700 first-letter:uppercase sm:inline-block">
-          {status}
-        </span>
-      </div>
+    <li key={serviceName} className="flex justify-between">
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <div className="flex items-center space-x-4">
+              <Checkbox
+                id={serviceName}
+                checked={checked[serviceName]}
+                onCheckedChange={(checkedVal) =>
+                  setChecked((old) => ({ ...old, [serviceName]: !!checkedVal }))
+                }
+              />
+              <span className={cn("h-3 w-3 rounded-full", color)}>
+                <span className="sr-only">{status.status}</span>
+              </span>
+              <label htmlFor={serviceName} className="first-letter:uppercase">
+                {serviceName}
+              </label>
+            </div>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>{status.message || "Healthy"}</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
 
-      <span className="flex gap-1">
+      <div className="flex gap-1">
         <Button variant="link" className="font-semibold text-primary-300">
           <Share2 />
           Share
         </Button>
         <HideItem serviceName={serviceName} />
-      </span>
+      </div>
     </li>
   );
 }
