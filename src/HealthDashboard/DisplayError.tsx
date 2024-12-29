49ea2ad5c50245dd0fb794b9b9149aad8c3f45e1
Brad R <31802085+bnrobinson93@users.noreply.github.com>
2024-12-28 23:11:24 -0600

    Add global error handler

---
+++ src/HealthDashboard/DisplayError.tsx
@@ -0,0 +1,19 @@
+import { useToast } from "@/hooks/use-toast";
+import { useEffect } from "react";
+
+type Props = { error: string | null };
+
+function DisplayError({ error }: Props) {
+  const { toast } = useToast();
+
+  useEffect(() => {
+    if (error === "") return;
+    toast({
+      title: "An error occurred",
+      description: error,
+      variant: "destructive",
+    });
+  }, [error, toast]);
+}
+
+export default DisplayError;
